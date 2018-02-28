b754b4676e3c084f4a507a63528392d8d23afd1b
Jacek Szczurek <Jacek.Golebiowski-Szczurek@travelplanet.pl>
2018-02-28 12:47:22 +0100

    adding automapper, seeding database with sample data

---
+++ CarRentalApp/CarRentalApp/Migrations/20180228113022_SeedDatabase.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CarRentalApp.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Makes (Name) Values ('BMW')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) Values ('Audi')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) Values ('Skoda')");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('M140i', (Select ID from Makes where Name = 'BMW'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('M240i', (Select ID from Makes where Name = 'BMW'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('M3', (Select ID from Makes where Name = 'BMW'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('M4', (Select ID from Makes where Name = 'BMW'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('530e', (Select ID from Makes where Name = 'BMW'))");
+
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('A3', (Select ID from Makes where Name = 'Audi'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('A5', (Select ID from Makes where Name = 'Audi'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('Q7', (Select ID from Makes where Name = 'Audi'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('TT', (Select ID from Makes where Name = 'Audi'))");
+
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('Fabia', (Select ID from Makes where Name = 'Skoda'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('Octavia', (Select ID from Makes where Name = 'Skoda'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Models WHERE MakeId in ((Select ID from Makes where Name = 'BMW'), (Select ID from Makes where Name = 'Audi'), (Select ID from Makes where Name = 'Skoda')");
+            migrationBuilder.Sql("Delete from Makes WHERE Name in ('BMW', 'Audi', 'Skoda'");
+        }
+    }
+}
--- CarRentalApp/CarRentalApp/Startup.cs
+++ CarRentalApp/CarRentalApp/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
@@ -23,8 +24,15 @@ namespace CarRentalApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region DbContext
+
             services.AddDbContext<CarRentalDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("Default")));
+
+            #endregion
+
+            services.AddAutoMapper();
+
 
             services.AddMvc();
         }
